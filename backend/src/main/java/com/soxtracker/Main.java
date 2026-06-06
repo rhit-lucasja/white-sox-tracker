5f41d6e1980c4eec17ccce404871ae619134fe76
rhit-lucasja <lucasja@rose-hulman.edu>
2026-06-06 17:24:17 -0400

    swagger working now

--- backend/src/main/java/com/soxtracker/Main.java
+++ backend/src/main/java/com/soxtracker/Main.java
@@ -1,11 +1,23 @@
 package com.soxtracker;
 
 import io.javalin.Javalin;
+import io.javalin.openapi.plugin.OpenApiPlugin;
+import io.javalin.openapi.plugin.swagger.SwaggerPlugin;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Javalin app = Javalin.create();
+		Javalin app = Javalin.create(config -> {
+			config.registerPlugin(new OpenApiPlugin(openApi ->
+				openApi.withDefinitionConfiguration((version, def) ->
+					def.withInfo(info -> {
+						info.title("Sox Tracker API");
+						info.version("0.0.1");
+					})
+				)
+			));
+			config.registerPlugin(new SwaggerPlugin());
+		});
 		app.get("/api/health", ctx -> ctx.result("ok"));
 		app.start(8080);
 	}
